55f9d5c4caf7d0d6edfee59836e1315456656828
Greet De Baets <greetdebaets@Greets-MacBook-Pro.local>
2017-02-03 16:12:31 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# vsearch-test
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: vsearch-test
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
