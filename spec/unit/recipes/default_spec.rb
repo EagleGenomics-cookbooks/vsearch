c17503da094d0cfb3614606c3f799d16ef3d47ab
GreetDB <grdbaets@gmail.com>
2017-02-07 15:46:50 +0000

    remove old reference to vsearch-test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,12 +1,12 @@
 #
-# Cookbook:: vsearch-test
+# Cookbook:: vsearch
 # Spec:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 require 'spec_helper'
 
-describe 'vsearch-test::default' do
+describe 'vsearch::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new
