711c380ec418e1befb8cd296318531f3035fda0f
GreetDB <grdbaets@gmail.com>
2017-02-08 16:20:01 +0000

    add test file to run additional test commands for vsearch

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,6 +5,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
+describe file '/tmp/reads.gz' do
+  it { should exist }
+end
+
 describe file '/usr/local/vsearch-2.3.4' do
   it { should be_directory }
 end
@@ -19,4 +23,14 @@ end
 
 describe command('. /etc/profile; vsearch') do
   its('stderr') { should match(/v2.3.4_linux_x86_64/) }
+end
+
+describe command('. /etc/profile; '\
+'vsearch --fastq_filter /tmp/reads.gz '\
+'--fastq_trunclen 95 --fastaout /tmp/test.fa') do
+  its('exit_status') { should eq 0 }
+end
+
+describe file('/tmp/test.fa') do
+  it { should exist }
 end
