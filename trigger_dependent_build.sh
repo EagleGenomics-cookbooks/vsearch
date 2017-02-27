0dec852acc88783134937f3984c6e29505e2a825
GreetDB <grdbaets@gmail.com>
2017-02-27 11:35:44 +0000

    add token

--- trigger_dependent_build.sh
+++ trigger_dependent_build.sh
@@ -1,13 +1,16 @@
+TOKEN=$(travis token --org)
+
 body='{
 "request": {
-  "branch":"master"
+  "branch":"master",
+  "message": "test"
 }}'
 
 curl -s -X POST \
   -H "Content-Type: application/json" \
   -H "Accept: application/json" \
   -H "Travis-API-Version: 3" \
-  -H "Authorization: token xxxxxx" \
+  -H "Authorization: token ${TOKEN}" \
   -d "$body" \
   https://api.travis-ci.com/repo/EagleGenomics-cookbooks%2Fvsearch_runnable/requests
 
